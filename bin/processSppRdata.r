8ceb128f1b5fcb14b5ac754131c615ec4af3c3b8
Chuan Wang <chuan.wang@scilifelab.se>
2018-08-17 15:59:48 +0200

    Small refactor

--- bin/processSppRdata.r
+++ bin/processSppRdata.r
@@ -8,13 +8,13 @@ args <- commandArgs(trailingOnly=TRUE)
 
 # Check input args
 if (length(args) != 2) {
-  stop("Usage: processSppRdata.r [ cross-correlation.Rdata ] [ cross-correlation.csv ]", call.=FALSE)
+  stop("Usage: processSppRdata.r [ input.Rdata ] [ output.csv ]", call.=FALSE)
 }
 
 load(args[1])
 data<-crosscorr$cross.correlation
 data$peak<-""
-data[data$x %in% crosscorr$phantom.cc$x,]$peak="phantom"
-data[data$x %in% cc.peak$x,]$peak="peak"
+data[data$x==crosscorr$phantom.cc$x,]$peak="phantom"
+data[data$x==cc.peak[cc.peak$y==max(cc.peak$y),]$x,]$peak="peak"
 
 write.csv(data, file=args[2], quote=FALSE, row.names=FALSE)
