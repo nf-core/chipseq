6428b3f9a18f75a711716d364c2f88f80e9ccf36
Harshil Patel <drpatelhh@gmail.com>
2022-10-02 09:31:06 +0100

    Black

--- bin/igv_files_to_session.py
+++ bin/igv_files_to_session.py
@@ -17,20 +17,25 @@ import argparse
 ############################################
 
 Description = 'Create IGV session file from a list of files and associated colours - ".bed", ".bw", ".bigwig", ".tdf", ".gtf" files currently supported.'
-Epilog = """Example usage: python igv_files_to_session.py <XML_OUT> <LIST_FILE> <GENOME>"""
+Epilog = (
+    """Example usage: python igv_files_to_session.py <XML_OUT> <LIST_FILE> <GENOME>"""
+)
 
 argParser = argparse.ArgumentParser(description=Description, epilog=Epilog)
 
 ## REQUIRED PARAMETERS
 argParser.add_argument("XML_OUT", help="XML output file.")
 argParser.add_argument(
-    "LIST_FILE", help="Tab-delimited file containing two columns i.e. file_name\tcolour. Header isnt required."
+    "LIST_FILE",
+    help="Tab-delimited file containing two columns i.e. file_name\tcolour. Header isnt required.",
 )
 argParser.add_argument(
-    "REPLACE_FILE", help="Tab-delimited file containing two columns i.e. file_name\treplacement_file_name. Header isnt required."
+    "REPLACE_FILE",
+    help="Tab-delimited file containing two columns i.e. file_name\treplacement_file_name. Header isnt required.",
 )
 argParser.add_argument(
-    "GENOME", help="Full path to genome fasta file or shorthand for genome available in IGV e.g. hg19."
+    "GENOME",
+    help="Full path to genome fasta file or shorthand for genome available in IGV e.g. hg19.",
 )
 
 ## OPTIONAL PARAMETERS
@@ -82,7 +87,6 @@ def igv_files_to_session(XMLOut, ListFile, ReplaceFile, Genome, PathPrefix=""):
         else:
             break
             fin.close()
-
     fileList = []
     fin = open(ListFile, "r")
     while True:
@@ -91,22 +95,24 @@ def igv_files_to_session(XMLOut, ListFile, ReplaceFile, Genome, PathPrefix=""):
             ifile, colour = line.strip().split("\t")
             if len(colour.strip()) == 0:
                 colour = "0,0,178"
-            for ofile,rfile in replaceFileDict.items():
+            for ofile, rfile in replaceFileDict.items():
                 if ofile in ifile:
                     ifile = ifile.replace(ofile, rfile)
             fileList.append((PathPrefix.strip() + ifile, colour))
         else:
             break
             fin.close()
-    
-    fout = open('igv_files.txt', "w")
+    fout = open("igv_files.txt", "w")
     for ifile, colour in fileList:
-        fout.write(ifile + '\n')
+        fout.write(ifile + "\n")
     fout.close()
 
     ## ADD RESOURCES SECTION
     XMLStr = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n'
-    XMLStr += '<Session genome="%s" hasGeneTrack="true" hasSequenceTrack="true" locus="All" version="8">\n' % (Genome)
+    XMLStr += (
+        '<Session genome="%s" hasGeneTrack="true" hasSequenceTrack="true" locus="All" version="8">\n'
+        % (Genome)
+    )
     XMLStr += "\t<Resources>\n"
     for ifile, colour in fileList:
         XMLStr += '\t\t<Resource path="%s"/>\n' % (ifile)
@@ -143,7 +149,9 @@ def igv_files_to_session(XMLOut, ListFile, ReplaceFile, Genome, PathPrefix=""):
                 '\t\t<Track altColor="0,0,178" autoScale="false" clazz="org.broad.igv.track.FeatureTrack" color="%s" '
                 % (colour)
             )
-            XMLStr += 'displayMode="COLLAPSED" featureVisibilityWindow="-1" fontSize="10" '
+            XMLStr += (
+                'displayMode="COLLAPSED" featureVisibilityWindow="-1" fontSize="10" '
+            )
             XMLStr += (
                 'id="%s" name="%s" renderer="BASIC_FEATURE" sortable="false" visible="true" windowFunction="count"/>\n'
                 % (ifile, os.path.basename(ifile))
@@ -160,7 +168,6 @@ def igv_files_to_session(XMLOut, ListFile, ReplaceFile, Genome, PathPrefix=""):
                 'id="%s" name="%s" renderer="BASIC_FEATURE" sortable="false" visible="true" windowFunction="count"/>\n'
                 % (ifile, os.path.basename(ifile))
             )
-
     XMLStr += "\t</Panel>\n"
     # XMLStr += '\t<HiddenAttributes>\n\t\t<Attribute name="DATA FILE"/>\n\t\t<Attribute name="DATA TYPE"/>\n\t\t<Attribute name="NAME"/>\n\t</HiddenAttributes>\n'
     XMLStr += "</Session>"
@@ -175,7 +182,13 @@ def igv_files_to_session(XMLOut, ListFile, ReplaceFile, Genome, PathPrefix=""):
 ############################################
 ############################################
 
-igv_files_to_session(XMLOut=args.XML_OUT, ListFile=args.LIST_FILE, ReplaceFile=args.REPLACE_FILE, Genome=args.GENOME, PathPrefix=args.PATH_PREFIX)
+igv_files_to_session(
+    XMLOut=args.XML_OUT,
+    ListFile=args.LIST_FILE,
+    ReplaceFile=args.REPLACE_FILE,
+    Genome=args.GENOME,
+    PathPrefix=args.PATH_PREFIX,
+)
 
 ############################################
 ############################################
